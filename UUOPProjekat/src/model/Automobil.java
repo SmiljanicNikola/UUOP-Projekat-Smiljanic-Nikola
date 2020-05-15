04873c5fccedb2b42e497d316fe2a1113ac246c7
Nikola Smiljanic <nikola.kiki.00@gmail.com>
2020-05-15 22:05:11 +0200

    Kreirana klasa Automobil sa pratecim metodama(Konstruktor sa i bez parametara, getteri i setteri kao i toString metoda.

---
+++ UUOPProjekat/src/model/Automobil.java
@@ -0,0 +1,111 @@
+package model;
+
+public class Automobil {
+	
+	public int id;
+	public Musterija vlasnik;
+	public String marka;
+	public String model;
+	public int godinaProizvodnje;
+	public String zapreminaMotora;
+	public String snagaMotora;
+	public String vrstaGoriva;
+	
+	public Automobil() {
+		this.id = 0;
+		this.vlasnik = new Musterija();
+		this.marka = "";
+		this.model = "";
+		this.godinaProizvodnje = 0;
+		this.zapreminaMotora = "";
+		this.snagaMotora = "";
+		this.vrstaGoriva = "";
+	}
+
+	public Automobil(int id, Musterija vlasnik, String marka, String model, int godinaProizvodnje,
+			String zapreminaMotora, String snagaMotora, String vrstaGoriva) {
+		super();
+		this.id = id;
+		this.vlasnik = vlasnik;
+		this.marka = marka;
+		this.model = model;
+		this.godinaProizvodnje = godinaProizvodnje;
+		this.zapreminaMotora = zapreminaMotora;
+		this.snagaMotora = snagaMotora;
+		this.vrstaGoriva = vrstaGoriva;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Musterija getVlasnik() {
+		return vlasnik;
+	}
+
+	public void setVlasnik(Musterija vlasnik) {
+		this.vlasnik = vlasnik;
+	}
+
+	public String getMarka() {
+		return marka;
+	}
+
+	public void setMarka(String marka) {
+		this.marka = marka;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getGodinaProizvodnje() {
+		return godinaProizvodnje;
+	}
+
+	public void setGodinaProizvodnje(int godinaProizvodnje) {
+		this.godinaProizvodnje = godinaProizvodnje;
+	}
+
+	public String getZapreminaMotora() {
+		return zapreminaMotora;
+	}
+
+	public void setZapreminaMotora(String zapreminaMotora) {
+		this.zapreminaMotora = zapreminaMotora;
+	}
+
+	public String getSnagaMotora() {
+		return snagaMotora;
+	}
+
+	public void setSnagaMotora(String snagaMotora) {
+		this.snagaMotora = snagaMotora;
+	}
+
+	public String getVrstaGoriva() {
+		return vrstaGoriva;
+	}
+
+	public void setVrstaGoriva(String vrstaGoriva) {
+		this.vrstaGoriva = vrstaGoriva;
+	}
+
+	@Override
+	public String toString() {
+		return "Automobil [id=" + id + ", vlasnik=" + vlasnik + ", marka=" + marka + ", model=" + model
+				+ ", godinaProizvodnje=" + godinaProizvodnje + ", zapreminaMotora=" + zapreminaMotora + ", snagaMotora="
+				+ snagaMotora + ", vrstaGoriva=" + vrstaGoriva + "]";
+	}
+	
+	
+	
+}
