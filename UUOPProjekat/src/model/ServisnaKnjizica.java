c2bf2c744331da5393892ad5cd7f21c4a7df01f7
Nikola Smiljanic <nikola.brebre6@gmail.com>
2020-05-15 23:41:38 +0200

    Kreirana klasa ServisnaKnjizica sa svim pratecim metodama i atributima

---
+++ UUOPProjekat/src/model/ServisnaKnjizica.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServisnaKnjizica {
+	
+	public int id;
+	public Automobil vlasnistvo;
+	public List<ServisAutomobila> listaObavljenihServisa;
+	
+	public ServisnaKnjizica() {
+		this.id = 0;
+		this.vlasnistvo = new Automobil();
+		this.listaObavljenihServisa = new ArrayList<ServisAutomobila>();
+	}
+
+	public ServisnaKnjizica(int id, Automobil vlasnistvo, List<ServisAutomobila> listaObavljenihServisa) {
+		super();
+		this.id = id;
+		this.vlasnistvo = vlasnistvo;
+		this.listaObavljenihServisa = listaObavljenihServisa;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Automobil getVlasnistvo() {
+		return vlasnistvo;
+	}
+
+	public void setVlasnistvo(Automobil vlasnistvo) {
+		this.vlasnistvo = vlasnistvo;
+	}
+
+	public List<ServisAutomobila> getListaObavljenihServisa() {
+		return listaObavljenihServisa;
+	}
+
+	public void setListaObavljenihServisa(List<ServisAutomobila> listaObavljenihServisa) {
+		this.listaObavljenihServisa = listaObavljenihServisa;
+	}
+
+	@Override
+	public String toString() {
+		return "ServisnaKnjizica [id=" + id + ", vlasnistvo=" + vlasnistvo + ", listaObavljenihServisa="
+				+ listaObavljenihServisa + "]";
+	}
+	
+	
+	
+}
